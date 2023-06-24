72db2eb3e9d568c2926a1bafffb87f399cc21e16
Mackenzie Kroon <kroonmackenzie@gmail.com>
2023-06-23 17:42:35 -0700

    title setup

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,17 +1,19 @@
 import React from "react";
 import name from "./images/name-vector.png"
 import engineer from "./images/software-engineer-vector.png"
+import star from "./images/star-vector.png"
 
 const Main = () => {
   return (
     <div id="home" className="heading-container">
-      <div className="title">
+      <div className="leftMain">
         <img className="name" src={name} alt="name"/>
-        <img className="softwareEngineer" src={engineer} alt="software engineer"/>
+        <div className="softwareEngineer">
+          <img className="engineer" src={engineer} alt="software engineer"/>
+          <img className="star" src={star} alt="star"/>
+        </div>
       </div>
-      <div className="extras">
-
-      </div>
+      <div className="rightMain"></div>
     </div>
   );
 };
