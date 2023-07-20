63aab1cedd53eed9c332943b222a52f16fa4db92
Mackenzie Kroon <kroonmackenzie@gmail.com>
2023-07-20 16:00:15 -0700

    project in progress

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,16 +1,9 @@
 import React from "react";
-// import Seekr from "./newSEekr";
-// import PupQuiz from "./PupQuiz";
-// import Juicery from "./Juicery";
-// import Portfolio from "./Portfolio";
-// import NewApp from "./NewApp";
-// import TravelApp from "./TravelApp";
 import ProjectsHeading from "../Components/images/projects-heading-vector.png"
 import Project from "./Project";
 import data from "../projectData"
 
 function Projects() {
-  console.log("Data--->", data)
     return (
       <div>
         <div id="projects" className="projects-container">
@@ -18,24 +11,20 @@ function Projects() {
           <div className="projects">
               { Object.entries(data).map(([project, value]) => {
                 return (
-                  <Project
-                  projectImage={value.projectImage.default}
-                  projectTitle={value.projectTitle}
-                  projectDescription={value.projectDescription}
-                  tech={value.tech}
-                  projectLink={value.projectLink}
-                  youtubeLink={value.youtubeLink}
-                  githubLink={value.githubLink}
-                  addInfoHeading={value.addInfoHeading}
-                  addInfoContent={value.addInfoContent}
-
-              />
+                    <Project
+                    projectImage={value.projectImage.default}
+                    projectTitle={value.projectTitle}
+                    projectDescription={value.projectDescription}
+                    tech={value.tech}
+                    projectLink={value.projectLink}
+                    youtubeLink={value.youtubeLink}
+                    githubLink={value.githubLink}
+                    addInfoHeading={value.addInfoHeading}
+                    addInfoContent={value.addInfoContent}
+                  />
                 )
-
-
               })
               }
-
           </div>
         </div>
       </div>
--- src/projectData.js
+++ src/projectData.js
@@ -1,5 +1,7 @@
 let seekrImage = require('./Components/images/seekr-vector.png')
-
+let pupquizImage = require('./Components/images/pupquiz-vector.png')
+let juiceryImage = require('./Components/images/juicery-vector.png')
+let vacayImage = require('./Components/images/travel-app-vector.png')
 
 const data = {
   'seekr': {
@@ -13,7 +15,29 @@ const data = {
     addInfoHeading: 'This app was designed mobile first',
     addInfoContent: 'For optimal user experience, please view on a mobile device'
   },
-
+  'Pup Quiz': {
+    projectImage: pupquizImage,
+    projectTitle: 'PupQuiz',
+    projectDescription: `A mobile app that tests users' knowledge of dog breeds`,
+    tech: ['React Native', 'JavaScript', 'Node', 'Express', 'PostgreSQL'],
+    githubLink: 'https://github.com/mackenziekroon/stackathon_project'
+  },
+  'Fruitify Juicery': {
+    projectImage: juiceryImage,
+    projectTitle: 'Fruitify Juicery',
+    projectDescription: 'A mock e-commerce web application that sells juices',
+    tech: ['JavaScript', 'React', 'Redux', 'Node', 'Express', 'PostgreSQL'],
+    projectLink: 'https://graceshopper-juicify.herokuapp.com/',
+    githubLink: 'https://github.com/grace-shopper-fruit-for-loops/graceshopper'
+  },
+  'Vacay': {
+    projectImage: vacayImage,
+    projectTitle: 'Vacay',
+    projectDescription: 'A website that helps you choose your next travel destination',
+    tech: ['JavaScript', 'HTML', 'CSS'],
+    projectLink: 'https://mackenziekroon.github.io/travel-app/',
+    githubLink: 'https://github.com/mackenziekroon/travel-app',
+  }
 }
 
 export default  data
